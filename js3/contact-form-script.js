7e99f2020382b4c7651ec018236234929fdb6c8f
deleg <pceletronicas525@hotmail.com>
2021-02-17 09:05:58 +0000

    Melhorar reserva do Alojamento2

--- js3/contact-form-script.js
+++ js3/contact-form-script.js
@@ -2,7 +2,7 @@ $("#contactForm").validator().on("submit", function (event) {
     if (event.isDefaultPrevented()) {
         // handle the invalid form...
         formError();
-        submitMSG(false, "Did you fill in the form properly?");
+        submitMSG(false, "Preencheu o formulário corretamente?");
     } else {
         // everything looks good!
         event.preventDefault();
