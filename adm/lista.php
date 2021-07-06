f7bccfc68f2d275019bfd8f57f5583f4d8515603
pcinformatica <pceletronicas525@hotmail.com>
2021-07-06 16:42:40 +0100

    lista(upload imagem)

--- adm/lista.php
+++ adm/lista.php
@@ -7,7 +7,10 @@
 while($arquivo = $diretorio->read()){
     if($arquivo != '.' && $arquivo != '..'){
         echo "<a href='".$pasta.$arquivo."'><img src='".$pasta.$arquivo."' width='50'>".$arquivo."</a><br>";
-    }
 
+    }
+?>
+    <a href="unlink.php?img=<?php echo $pasta.$arquivo; ?>"> Exluir arquivo</a>
+<?php
 }
 ?>
